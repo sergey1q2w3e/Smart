f49e648b499d615af2dc98d186b0bbec0fedead9
Сергей Прыгунов <sergey1q2w3e@gmail.com>
2017-12-19 22:39:31 +0300

    add statHTController

---
+++ SmartHouseService/SmartHouseService/DataObjects/StatisticsHT.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+
+namespace SmartHouseService.DataObjects
+{
+    public class StatisticsHT : EntityData
+    {
+        public int Humidity { get; set; }
+        public DateTime HumidityDateTime { get; set; }
+        public int Temperature { get; set; }
+        public DateTime TemperatureDateTime { get; set; }
+    }
+}
